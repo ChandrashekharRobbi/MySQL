bdb24e05fbab6f2e16488a6b557a8cb0083c9d18
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-26 13:17:59 +0530

    backslash used

--- Refining Selections/Escaping Wild Cards.sql
+++ Refining Selections/Escaping Wild Cards.sql
@@ -1,1 +1,9 @@
 -- Escaping Wild Cards
+/* what if we have to search '%' we can't do because of wildcards
+   so we can use '\' which is also called backslash 
+   and perform the operations
+*/
+SELECT * FROM BOOKS WHERE title LIKE '%\%%';
+-- it means we have to check for percenatage sign in middle of anything between title
+-- we can also search if we know exact positions
+SELECT * FROM BOOKS WHERE title LIKE '__\%%';
