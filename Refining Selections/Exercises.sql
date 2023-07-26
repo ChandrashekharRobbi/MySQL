34dd83639d3e17f4e34323a1cf7d51f82d917718
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-26 13:47:40 +0530

    Solved Exercises

---
+++ Refining Selections/Exercises.sql
@@ -0,0 +1,25 @@
+-- Exercise
+
+-- Select titles that contains 'stories'
+SELECT title FROM BOOKS WHERE title LIKE '%stories%';
+
+/*Find The Longest Book
+Print Out the Title and Page Count*/
+SELECT title, pages FROM BOOKS ORDER BY pages DESC LIMIT 1;
+
+/* Print a summary containing the title and
+year, for the 3 most recent books */
+SELECT CONCAT(title,'-', released_year) AS summary FROM books 
+ORDER BY released_year DESC LIMIT 3;
+
+/* That contains space */
+SELECT title, author_lname FROM BOOKS WHERE author_lname LIKE '% %';
+
+-- lowest stock quantity
+SELECT title, released_year, stock_quantity FROM BOOKS ORDER BY stock_quantity, released_year DESC LIMIT 4;
+
+-- first by author_lname and then title
+SELECT title, author_lname FROM BOOKS ORDER BY author_lname, title;
+
+-- custom text sort by author lname
+SELECT CONCAT("My Favourite AUTHOR IS ",author_fname," ",author_lname,"!") AS yell FROM BOOKS ORDER BY author_lname;
