441ee0b03433e9ad039608e583b8dd626a33e85f
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-26 12:51:29 +0530

    Created LIMIT

---
+++ Refining Selections/LIMIT.sql
@@ -0,0 +1,6 @@
+-- LIMIT
+-- we can limit the number rows we get
+SELECT * FROM BOOKS ORDER BY title LIMIT 5; -- WE GET TOP 5 TITLE BOOKS
+SELECT title, released_year FROM BOOKS ORDER BY released_year LIMIT 5; -- TOP 5 BOOKS BY RELEASED YEAR
+
+ 
