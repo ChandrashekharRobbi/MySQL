c7a1d7ae36d6603738f12577ed7506058662585d
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-26 15:07:55 +0530

    GROUP BY all possibilities

--- Aggregate Functions/GROUP BY.sql
+++ Aggregate Functions/GROUP BY.sql
@@ -1,1 +1,6 @@
 -- GROUP BY
+-- it aggregates and summarizes the data into single identical rows
+SELECT author_lname FROM BOOKS GROUP BY author_lname;
+-- Here each name has there own specific group
+-- Lets count how much book each group contains
+SELECT author_lname, COUNT(*) FROM BOOKS GROUP BY author_lname;
