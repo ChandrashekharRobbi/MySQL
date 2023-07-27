b8d780f88622713b65fe3c6bd295519a456ad3e3
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 12:58:04 +0530

    Numbers ✅

--- Data Types/Numbers.sql
+++ Data Types/Numbers.sql
@@ -1,1 +1,13 @@
 -- Numbers
+/*
+Type	Storage (Bytes)	Maximum Value Signed
+TINYINT		1				127	
+SMALLINT	2				32767	
+MEDIUMINT	3				8388607	
+INT			4				2147483647	
+BIGINT		8				2 ** 63 -1	
+https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+*/
+/*
+DEPENDING UPON THE NEED WE CAN USE ABOVE INTEGER TYPE
+*/
