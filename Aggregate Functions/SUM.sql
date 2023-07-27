c45cef0d7418dada8d621062075a95fe932ca28b
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 11:39:00 +0530

    SUM ✅

--- Aggregate Functions/SUM.sql
+++ Aggregate Functions/SUM.sql
@@ -1,1 +1,11 @@
 -- SUM
+SELECT SUM(pages) AS total_pages FROM BOOKS;
+
+-- SUM + GroupBy
+-- Sum all pages each author has written
+SELECT 
+	CONCAT(author_fname, ' ', author_lname) AS Name,
+    SUM(pages) AS Total_pages_of_each_author
+FROM BOOKS GROUP BY Name;
+
+-- ‼️NOTE: SUM(ofString) is always equal to 0
