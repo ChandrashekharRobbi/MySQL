ef312d892979cd040207c2944eefb2f5f2599935
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 10:56:33 +0530

    Subqueries ✅

--- Aggregate Functions/SUBQUERIES.sql
+++ Aggregate Functions/SUBQUERIES.sql
@@ -5,3 +5,9 @@ SELECT title, pages FROM BOOKS ORDER BY pages DESC LIMIT 1;
 -- APPROACH 2 (Subqueries 🔥)
 SELECT title, pages FROM BOOKS 
 WHERE pages = (SELECT MAX(PAGES) FROM BOOKS);
+-- Working Right 😀 But there is a catch 👀
+-- it will be ok if we know there is only one book what if it has 
+-- multiple books of same max page?
+-- it will display both of them
+SELECT title, pages FROM BOOKS 
+WHERE pages = (SELECT MAX(PAGES) FROM BOOKS);
