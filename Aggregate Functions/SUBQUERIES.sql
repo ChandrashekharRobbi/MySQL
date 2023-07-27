068d5aa9750c14fe10229ff375947bf9c988f9d9
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 10:49:31 +0530

    subqueries are intersting 🔥

--- Aggregate Functions/SUBQUERIES.sql
+++ Aggregate Functions/SUBQUERIES.sql
@@ -1,1 +1,7 @@
 -- SUBQUERIES
+-- What will be the title of the longest pages book?
+-- APPROACH 1 (Not gives directly)
+SELECT title, pages FROM BOOKS ORDER BY pages DESC LIMIT 1;
+-- APPROACH 2 (Subqueries 🔥)
+SELECT title, pages FROM BOOKS 
+WHERE pages = (SELECT MAX(PAGES) FROM BOOKS);
