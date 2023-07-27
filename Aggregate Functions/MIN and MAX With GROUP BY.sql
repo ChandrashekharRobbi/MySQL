da46e8d45f74bcfe11c5e262044792cb45f725df
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 11:28:32 +0530

    MIN and MAX With GROUP BY ✅

--- Aggregate Functions/MIN and MAX With GROUP BY.sql
+++ Aggregate Functions/MIN and MAX With GROUP BY.sql
@@ -1,1 +1,41 @@
 -- MIN and MAX With GROUP BY
+-- Find the year where each author published there first book?📚
+SELECT 
+	author_fname, 
+    author_lname, 
+    MIN(released_year) 
+FROM BOOKS group by author_lname, author_fname;
+-- Find the year where each author published there recent book?
+SELECT 
+	author_fname, 
+    author_lname, 
+    MAX(released_year)
+FROM BOOKS group by author_lname, author_fname;
+-- Find the year where each author published there first and recent book?
+SELECT 
+	author_fname, 
+    author_lname, 
+    MIN(released_year) AS First_Book,
+    MAX(released_year) AS Recent_Book
+FROM BOOKS group by author_lname, author_fname;
+-- Find the year where each author published 
+-- there first and recent book as well as no of books published?
+SELECT 
+	author_fname, 
+    author_lname, 
+    COUNT(*) AS no_of_book_published,
+    MIN(released_year) AS First_Book,
+    MAX(released_year) AS Recent_Book
+FROM BOOKS group by author_lname, author_fname;
+-- Find the year where each author published 
+-- there first and recent book as well as no of books published
+-- and longest page count ?
+
+SELECT 
+	author_fname, 
+    author_lname, 
+    COUNT(*) AS no_of_book_published,
+    MIN(released_year) AS First_Book,
+    MAX(released_year) AS Recent_Book,
+    MAX(pages) AS longest_page
+FROM BOOKS group by author_lname, author_fname;
