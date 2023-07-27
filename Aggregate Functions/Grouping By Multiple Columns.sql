bda2955909ade6f548d87633747ca9ba75706276
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 11:10:49 +0530

    Grouping By Multiple Columns ✅

--- Aggregate Functions/Grouping By Multiple Columns.sql
+++ Aggregate Functions/Grouping By Multiple Columns.sql
@@ -1,1 +1,10 @@
+SELECT * FROM BOOKS;
 -- Grouping By Multiple Columns
+SELECT author_fname, author_lname , COUNT(*) 
+FROM BOOKS GROUP BY author_lname, author_fname;
+-- Here we have GROUP BY on the basis of 2 factors
+
+-- we can also do similar thing by using CONCAT()
+SELECT CONCAT(author_fname, ' ', author_lname) AS Name
+,Count(*) AS count 
+FROM BOOKS GROUP BY Name;
