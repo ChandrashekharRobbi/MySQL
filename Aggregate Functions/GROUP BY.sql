cfc245d6bcc168c76d825cabd4a7ebd7a0e72bc6
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 10:32:14 +0530

    Group BY Concepts

--- Aggregate Functions/GROUP BY.sql
+++ Aggregate Functions/GROUP BY.sql
@@ -4,3 +4,14 @@ SELECT author_lname FROM BOOKS GROUP BY author_lname;
 -- Here each name has there own specific group
 -- Lets count how much book each group contains
 SELECT author_lname, COUNT(*) FROM BOOKS GROUP BY author_lname;
+
+-- Sort according to books
+SELECT author_lname, COUNT(*) AS books_written 
+FROM BOOKS GROUP BY author_lname ORDER BY books_written;
+
+-- Now lets start by using released Year
+SELECT released_year FROM books GROUP BY released_year;
+
+
+SELECT released_year, COUNT(*) AS count_of_books
+FROM books GROUP BY released_year ORDER BY count_of_books DESC;
