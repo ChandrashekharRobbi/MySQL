d2ce0571be923ffc6464994c31ce0b38c3244495
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 11:46:53 +0530

    AVG ✅

--- Aggregate Functions/AVG.sql
+++ Aggregate Functions/AVG.sql
@@ -1,1 +1,23 @@
 -- AVG
+-- Calculate average released year across all books
+SELECT AVG(released_year) FROM BOOKS;
+-- Calculate average pages across all books
+SELECT AVG(pages) FROM BOOKS;
+-- Calculate average stock_quantity across all books
+SELECT AVG(stock_quantity) FROM BOOKS;
+-- Calculate average stock_quantity for books released in the same year
+SELECT 
+	released_year,
+    AVG(stock_quantity) AS avg_stock_quantity
+FROM BOOKS 
+	GROUP BY(released_year) 
+    ORDER BY released_year;
+-- Calculate average stock_quantity for books 
+-- released in the same year and count too
+SELECT 
+	released_year,
+    AVG(stock_quantity) AS avg_stock_quantity,
+    COUNT(*) 
+FROM BOOKS 
+	GROUP BY(released_year) 
+    ORDER BY released_year;
