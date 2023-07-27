d5f4c3cbe80b5175349509ff5260ccbe637ed006
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-27 11:01:40 +0530

    Group By ✅

--- Aggregate Functions/GROUP BY.sql
+++ Aggregate Functions/GROUP BY.sql
@@ -12,6 +12,6 @@ FROM BOOKS GROUP BY author_lname ORDER BY books_written;
 -- Now lets start by using released Year
 SELECT released_year FROM books GROUP BY released_year;
 
-
+-- Group by and order by
 SELECT released_year, COUNT(*) AS count_of_books
 FROM books GROUP BY released_year ORDER BY count_of_books DESC;
