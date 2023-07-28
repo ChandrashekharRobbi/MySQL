1d8d83cac650a0cac99f922d2fd4eee43a5556cb
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-28 15:10:25 +0530

    Date Functions ✅

--- Data Types/Date Functions.sql
+++ Data Types/Date Functions.sql
@@ -1,1 +1,12 @@
 -- Date Functions
+-- DAY() will extract date from the DATE
+SELECT birthdate, DAY(birthdate) FROM people;
+-- DAYOFWEEK() will tell which day it will be
+-- by using numbers 1 = Sunday , 2 = Monday and so on till 7
+SELECT birthdate, DAY(birthdate), DAYOFWEEK(birthdate) FROM PEOPLE;
+-- DAYOFYEAR() will tell which day of the year it is from 1 to 366
+SELECT birthdate, DAY(birthdate), DAYOFWEEK(birthdate), DAYOFYEAR(birthdate) FROM PEOPLE;
+-- MONTHNAME() will give us the name of the month
+SELECT birthdate, DAY(birthdate), DAYOFWEEK(birthdate), DAYOFYEAR(birthdate) , MONTHNAME(birthdate) FROM PEOPLE;
+-- YEAR() will give us which week it was
+SELECT birthdate, YEAR(birthdate) FROM people;
