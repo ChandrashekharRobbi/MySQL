39ad98b7e141df728bb250497397e4343c7f1c09
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-28 15:20:10 +0530

    Time Functions ✅

--- Data Types/Time Functions.sql
+++ Data Types/Time Functions.sql
@@ -1,1 +1,11 @@
 -- Time Functions
+-- HOUR()
+SELECT birthtime, HOUR(birthtime) FROM PEOPLE;
+-- MINUTE()
+SELECT birthtime, MINUTE(birthtime) FROM PEOPLE;
+-- second()
+SELECT birthtime, SECOND(birthtime) FROM PEOPLE;
+-- Extract DATE from Timestamp()
+SELECT birthdt , DATE(birthdt) FROM PEOPLE;
+-- Extract TIME from Timestamp()
+SELECT birthdt , TIME(birthdt) FROM PEOPLE;
