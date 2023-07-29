c9edfdb94789a17468b69a50bf723ecbd12223eb
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-29 15:05:08 +0530

    Started 🚀

--- Comparison and Logical Operators/IN  and modulo Operator.sql
+++ Comparison and Logical Operators/IN  and modulo Operator.sql
@@ -2,4 +2,10 @@
 SELECT title, author_lname FROM BOOKS 
 WHERE author_lname IN ('Lahiri', 'Gaiman');
 
-SELECT author_lname FROM BOOKS;
+SELECT title, author_lname FROM BOOKS 
+WHERE author_lname NOT IN ('Lahiri', 'Gaiman');
+
+-- Modulo
+SELECT * FROM BOOKS 
+WHERE 
+released_year >= 2000 AND released_year % 2 != 0;
