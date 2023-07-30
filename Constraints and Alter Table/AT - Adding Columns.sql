cd836d264dc1bcb31d4b7842e296991120d45d10
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-30 15:52:32 +0530

    AT - Adding Columns ✅

--- Constraints and Alter Table/AT - Adding Columns.sql
+++ Constraints and Alter Table/AT - Adding Columns.sql
@@ -1,1 +1,15 @@
--- AT : Adding Columns
+-- AT - Adding Columns
+
+ALTER table COMPANIES
+ADD COLUMN CITY VARCHAR(25);
+-- IT WILL SET NULL AS A DEFAULT VALUE
+
+ALTER TABLE companies
+ADD COLUMN employee_count INT NOT NULL;
+-- now it will take DEFAULT value as 0
+
+ALTER TABLE companies
+ADD COLUMN employee_count2 INT NOT NULL DEFAULT 1;
+-- it will now take 1 as DEFAULT value
+
+SELECT * FROM COMPANIES;
