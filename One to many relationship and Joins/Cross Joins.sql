a10110428ab84cbaa1065991eede8840de7061d7
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 00:28:26 +0530

    Cross Joins ✅

--- One to many relationship and Joins/Cross Joins.sql
+++ One to many relationship and Joins/Cross Joins.sql
@@ -1,2 +1,17 @@
 -- Cross Joins
+SELECT * FROM CUSTOMERS;
+# get the value of George
+SELECT id FROM customers WHERE last_name = 'George';
+SELECT * FROM ORDERS WHERE customer_id = 1;
+
+-- OR WE CAN USE SUBQUERIES
+SELECT * FROM ORDERS 
+WHERE customer_id = (
+						SELECT id FROM customers 
+                        WHERE last_name = 'George'
+					);
+# cross joins
+# it will give all possibility of customers and orders
+# dumb joint
+SELECT * FROM customers,orders;
 
