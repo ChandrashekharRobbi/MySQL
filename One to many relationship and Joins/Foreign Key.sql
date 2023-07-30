d70743149f7b1ce8cded5d6a357e969b785bd069
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 00:10:00 +0530

    Foreign Key ✅

--- One to many relationship and Joins/Foreign Key.sql
+++ One to many relationship and Joins/Foreign Key.sql
@@ -1,1 +1,44 @@
 -- Foreign Key
+-- Creating Customers table
+CREATE TABLE customers(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+-- Creating Orders Table
+CREATE TABLE orders(
+	order_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE,
+	amount DECIMAL(8,2),  -- total 8 including after decimal 2
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+       
+       
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5);
+       
+SELECT * FROM CUSTOMERS;
+SELECT * FROM ORDERS;
+
+INSERT INTO ORDERS (order_date, amount, customer_id)
+VALUES ('2023-01-01',78.99, 234);
+-- ERROR
+
+INSERT INTO ORDERS (order_date, amount, customer_id)
+VALUES ('2023-01-01',78.99, 3);
+-- works 
