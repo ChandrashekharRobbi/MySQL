5b25db6478ddf9088879175c9d82ff21937c6dfc
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-30 15:42:46 +0530

    Multiple Column Constraints ✅

--- Constraints and Alter Table/Multiple Column Constraints.sql
+++ Constraints and Alter Table/Multiple Column Constraints.sql
@@ -1,1 +1,12 @@
 -- Multiple Column Constraints
+
+CREATE TABLE Companies(
+	name VARCHAR(100),
+    address VARCHAR(200),
+    CONSTRAINT name_address UNIQUE(name, address)
+);
+
+INSERT INTO Companies (name, address) VALUES ('john','123 johnny');
+
+INSERT INTO Companies (name, address) VALUES ('john','123 johnny');
+-- Error Code: 1062. Duplicate entry 'john-123 johnny' for key 'companies.name_address'
