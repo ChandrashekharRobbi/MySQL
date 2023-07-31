0e815b4418ed54557394f61b2ac870ee26b4ece4
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 10:38:52 +0530

    Inner Joins with Group By ✅

--- One to many relationship and Joins/Inner Joins with Group By.sql
+++ One to many relationship and Joins/Inner Joins with Group By.sql
@@ -1,1 +1,7 @@
 -- Inner Joins with Group By
+# get the total amount of each customers
+
+SELECT first_name, last_name, SUM(amount) AS total_amount FROM customers
+JOIN orders ON customers.id = orders.customer_id 
+GROUP BY first_name,last_name
+ORDER BY total_amount;
