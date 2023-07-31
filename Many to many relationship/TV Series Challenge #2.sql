b775f7e99928cf49ef3a96aa75d71d7c5f7163a0
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 12:24:59 +0530

    TV Series Challenge #2 ✅

--- Many to many relationship/TV Series Challenge #2.sql
+++ Many to many relationship/TV Series Challenge #2.sql
@@ -1,1 +1,6 @@
 -- TV Series Challenge #2
+
+SELECT title, ROUND(AVG(rating), 2) AS average_rating FROM series
+INNER JOIN reviews ON series.id = reviews.series_id
+GROUP BY title
+ORDER BY average_rating;
