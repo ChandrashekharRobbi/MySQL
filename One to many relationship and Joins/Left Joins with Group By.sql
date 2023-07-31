b603a705ed3323dfa8cd75582e0830b81b3b5ccd
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 10:52:11 +0530

    Left Joins with Group By ✅

--- One to many relationship and Joins/Left Joins with Group By.sql
+++ One to many relationship and Joins/Left Joins with Group By.sql
@@ -1,1 +1,6 @@
 -- Left Joins with Group By
+
+SELECT first_name, last_name , IFNULL(sum(amount), 0)	 as total_amount FROM customers
+LEFT JOIN orders ON orders.customer_id = customers.id 
+GROUP BY first_name, last_name
+ORDER BY total_amount;
