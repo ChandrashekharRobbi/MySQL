6fcaf63e4d4a14708a9192f1993c84aa21594976
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 13:13:30 +0530

    Completed 💖✅

--- Many to many relationship/TV Series Challenge #7.sql
+++ Many to many relationship/TV Series Challenge #7.sql
@@ -1,3 +1,15 @@
 -- TV Series Challenge #7
 
-# title, rating, 
+# title, rating, reviewer
+SELECT title FROM series;
+SELECT rating FROM reviews;
+SELECT CONCAT(first_name,' ', last_name) FROM reviewers;
+
+SELECT 
+    title,
+    rating,
+    CONCAT(first_name, ' ', last_name) AS full_name
+FROM
+    reviews INNER JOIN series ON reviews.series_id = series.id
+	INNER JOIN
+    reviewers ON reviews.reviewer_id = reviewers.id;
