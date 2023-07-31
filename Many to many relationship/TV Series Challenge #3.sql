40e2505a5a556737ba14aca43487205355fa6be9
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 12:30:10 +0530

    TV Series Challenge #3 ✅

--- Many to many relationship/TV Series Challenge #3.sql
+++ Many to many relationship/TV Series Challenge #3.sql
@@ -1,2 +1,10 @@
 -- TV Series Challenge #3
 
+SELECT first_name , last_name FROM reviewers;
+desc reviewers;
+
+SELECT rating FROM reviews;
+desc reviews;
+
+SELECT first_name, last_name , rating FROM reviews
+INNER JOIN reviewers ON reviews.reviewer_id = reviewers.id;
