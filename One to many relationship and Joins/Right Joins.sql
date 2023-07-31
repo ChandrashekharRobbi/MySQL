e3c443a209f68f6721271236de8ee252e0ae4d94
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 10:58:34 +0530

    Right Joins ✅

--- One to many relationship and Joins/Right Joins.sql
+++ One to many relationship and Joins/Right Joins.sql
@@ -1,1 +1,11 @@
 -- Right Joins
+select * from orders;
+select * from customers;
+
+SELECT first_name, last_name, amount FROM orders
+RIGHT JOIN customers ON customers.id = orders.customer_id;
+
+-- Group By
+SELECT first_name, last_name, sum(amount) as total_amount FROM orders
+RIGHT JOIN customers ON customers.id = orders.customer_id
+GROUP BY first_name, last_name;
