f7782bbdcd52ed6de47365807f6b9dd702df567a
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 10:21:51 +0530

    Inner Joins ✅

--- One to many relationship and Joins/Inner Joins.sql
+++ One to many relationship and Joins/Inner Joins.sql
@@ -1,1 +1,20 @@
 -- Inner Joins
+SELECT * FROM customers;
+SELECT * FROM ORDERS; 
+
+SELECT * FROM customers
+JOIN orders ON customers.id = orders.customer_id;
+
+-- OR
+
+SELECT * FROM customers
+JOIN orders ON orders.customer_id = customers.id;
+
+# get selected columns
+
+SELECT first_name, last_name, email, order_date,amount FROM customers
+JOIN orders ON orders.customer_id = customers.id;
+
+-- OR we can also write INNER JOIN
+SELECT first_name, last_name, amount FROM customers
+INNER JOIN orders ON customers.id = orders.customer_id;
