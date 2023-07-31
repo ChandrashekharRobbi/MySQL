7ce676cc7019a2624694a7cf79a0262a3db23a0e
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-07-31 10:47:53 +0530

    Left Joins ✅

--- One to many relationship and Joins/Left Joins.sql
+++ One to many relationship and Joins/Left Joins.sql
@@ -1,1 +1,5 @@
 -- Left Joins
+SELECT first_name, last_name, amount FROM customers -- (Left table)
+LEFT JOIN orders ON customers.id = orders.customer_id;
+	-- (right table)
+# it says select everything from left and matching records of right
