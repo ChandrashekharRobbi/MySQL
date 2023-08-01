18eae0343f3da5fbb56c8bf0a56c4f99b4154067
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-01 17:39:27 +0530

    Alterting Views ✅

--- Views and Modes/Alterting Views.sql
+++ Views and Modes/Alterting Views.sql
@@ -1,1 +1,12 @@
--- Alterting Views
+-- Replacing/Alterting Views
+
+CREATE OR REPLACE VIEW ordered_series AS
+SELECT * FROM SERIES ORDER BY genre;
+-- if it not exists then create if exists then replace
+
+# OR
+ALTER VIEW ordered_series AS 
+SELECT * FROM SERIES ORDER BY released_year;
+
+-- DROP VIEW
+DROP VIEW ordered_series;
