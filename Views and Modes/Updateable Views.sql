0834f9f4051529168120e99a9aa159a841cd1ff2
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-01 17:27:36 +0530

    Updateable Views ✅

--- Views and Modes/Updateable Views.sql
+++ Views and Modes/Updateable Views.sql
@@ -1,1 +1,17 @@
 -- Updateable Views
+
+-- Simple Table can be inserted and update the values
+CREATE VIEW review_order AS
+SELECT * from reviewers ORDER BY first_name;
+
+-- this table we can insert the values and delete it
+SELECT * from review_order;
+
+INSERT INTO review_order (first_name, last_name)
+VALUES ('Robbi','Car');
+
+SELECT * FROM review_order;
+
+DELETE FROM review_order WHERE id= 8;
+
+
