d778714dc3db6282b80befce4bb208bc902944b5
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-01 18:37:37 +0530

    More Modes ✅

--- Views and Modes/More Modes.sql
+++ Views and Modes/More Modes.sql
@@ -1,2 +1,25 @@
 -- More Modes
 
+SELECT @@SESSION.SQL_MODE;
+
+SET @@SESSION.SQL_MODE = 'ONLY_FULL_GROUP_BY';
+
+SELECT title, AVG(rating) FROM reviews
+JOIN Series ON series.id = reviews.series_id GROUP BY title;
+# now we cannot select rating column as it is not included in Group by condition
+# we will get error because of mode
+SELECT title, rating FROM reviews
+JOIN Series ON series.id = reviews.series_id GROUP BY title;
+
+# if we set null
+SET @@SESSION.SQL_MODE = '';
+
+SELECT title, rating FROM reviews
+JOIN Series ON series.id = reviews.series_id GROUP BY title;
+
+-- NO_ZERO_IN_DATE 
+/*
+	2000-00-10 :- VALID
+    0000-00-00 :- NOT VALID
+*/
+-- IF THERE IS A STRICT MODE ENABLED THEN THE EFFECT WILL BE DIFFERENT
