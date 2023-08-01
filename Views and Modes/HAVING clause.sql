bf1e7160c825a6af4249388571053eadc79c08f4
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-01 17:48:57 +0530

    HAVING clause ✅

--- Views and Modes/HAVING clause.sql
+++ Views and Modes/HAVING clause.sql
@@ -1,1 +1,9 @@
 -- HAVING clause
+SELECT * FROM full_reviews;
+
+SHOW TABLES;
+SELECT title, 
+	   AVG(rating) ,
+       COUNT(rating) AS rating_count
+FROM full_reviews 
+GROUP BY title HAVING COUNT(rating) >= 5;
