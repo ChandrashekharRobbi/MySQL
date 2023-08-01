3aa01ec3281e967349c12eab9b674c61af453f8d
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-01 18:16:10 +0530

    Modes ✅

--- Views and Modes/Modes.sql
+++ Views and Modes/Modes.sql
@@ -1,1 +1,12 @@
 -- MODES
+
+SELECT @@GLOBAL.SQL_MODE;
+SELECT @@SESSION.SQL_MODE;
+
+# lets remove zero one
+SET SESSION SQL_MODE = '';
+# it will remove all the modes
+
+SELECT 1/0;
+
+SHOW WARNINGS; -- NONE
