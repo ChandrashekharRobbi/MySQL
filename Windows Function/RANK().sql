f35d7705e7565b4fe253faeaa38e5e0363e8ce2d
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-02 16:26:38 +0530

    RANK() ✅

--- Windows Function/RANK().sql
+++ Windows Function/RANK().sql
@@ -1,1 +1,14 @@
 -- RANK()
+
+SELECT emp_no,
+	   department,
+       salary,
+       RANK() OVER(ORDER BY SALARY DESC)
+FROM employees;
+
+SELECT emp_no,
+       department,
+       salary,
+       RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_rank,
+       RANK() OVER(ORDER BY salary DESC) AS overall_salary_rank
+FROM EMPLOYEES ORDER BY department;
