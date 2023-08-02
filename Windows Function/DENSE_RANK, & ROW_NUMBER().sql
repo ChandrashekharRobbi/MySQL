f50cc2f771baa68705964ae1b590dda3b0d4cd36
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-02 16:42:25 +0530

    DENSE_RANK, & ROW_NUMBER() ✅

--- Windows Function/DENSE_RANK, & ROW_NUMBER().sql
+++ Windows Function/DENSE_RANK, & ROW_NUMBER().sql
@@ -1,1 +1,40 @@
 -- DENSE_RANK, & ROW_NUMBER()
+
+SELECT emp_no,
+	   department,
+       salary,
+       RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_rank,
+       RANK() OVER(ORDER BY salary DESC) AS overall_rank
+FROM EMPLOYEES;
+
+-- ROW_NUMBER()
+
+SELECT emp_no,
+	   department,
+       salary,
+       ROW_NUMBER() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_row_no,
+       RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_rank,
+       RANK() OVER(ORDER BY salary DESC) AS overall_rank
+FROM EMPLOYEES ORDER BY DEPARTMENT;
+
+-- DENSE_RANK() IT WILL NOT SKIP THE NUMBER AS THE RANK() DOES
+SELECT emp_no,
+	   department,
+       salary,
+       ROW_NUMBER() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_row_no,
+       RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_rank,
+       RANK() OVER(ORDER BY salary DESC) AS overall_rank,
+       DENSE_RANK() OVER(ORDER BY salary DESC) AS overall_dense_rank
+FROM EMPLOYEES ORDER BY overall_dense_rank;
+
+-- overall_row number
+
+SELECT emp_no,
+	   department,
+       salary,
+       ROW_NUMBER() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_row_no,
+       RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_rank,
+       RANK() OVER(ORDER BY salary DESC) AS overall_rank,
+       DENSE_RANK() OVER(ORDER BY salary DESC) AS overall_dense_rank,
+       ROW_NUMBER() OVER(ORDER BY salary DESC) AS overall_row_num
+FROM EMPLOYEES ORDER BY overall_dense_rank;
