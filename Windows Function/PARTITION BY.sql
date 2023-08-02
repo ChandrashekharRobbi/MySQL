967e8bc3709b73211932a63ce251caf393034921
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-02 16:02:57 +0530

    PARTITION BY ✅

--- Windows Function/PARTITION BY.sql
+++ Windows Function/PARTITION BY.sql
@@ -1,2 +1,20 @@
 -- PARTITION BY
 
+# if we place something in OVER() then it will acts like GROUP BY
+SELECT department,
+       salary,
+       AVG(salary) OVER(PARTITION BY department) AS dept_avg
+FROM employees;
+
+SELECT department,
+       salary,
+       AVG(salary) OVER(PARTITION BY department) AS dept_avg,
+       AVG(salary) OVER() AS company_avg
+FROM employees;
+
+# count how many employees in each department
+SELECT department,
+       salary,
+       Count(*) OVER(partition by department) AS dept_count
+FROM EMPLOYEES;
+       
