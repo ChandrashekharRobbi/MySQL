79a17eefdeb51f7c02ac4ca716210fc987ac4425
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-02 15:48:15 +0530

    OVER() ✅

--- Windows Function/OVER().sql
+++ Windows Function/OVER().sql
@@ -1,1 +1,16 @@
 -- OVER()
+
+-- it just distributes the aggregate functions to each row
+SELECT AVG(salary) FROM EMPLOYEES; -- Normal way
+
+SELECT AVG(salary) OVER() FROM EMPLOYEES; -- for each row
+
+SELECT emp_no, department, salary,AVG(salary) OVER()
+from employees;
+
+SELECT emp_no, 
+       department, 
+       salary,
+       MIN(salary) OVER(), 
+       MAX(salary) OVER()
+from employees;
