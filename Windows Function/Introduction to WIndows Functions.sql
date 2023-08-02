f2dc8698e601a62e1ecf24c8191704dc7d160a46
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-02 15:30:58 +0530

    Introduction to WIndows Functions ✅

--- Windows Function/Introduction to WIndows Functions.sql
+++ Windows Function/Introduction to WIndows Functions.sql
@@ -1,1 +1,21 @@
 -- Introduction to WIndows Functions
+/*
+	Window Functions perform aggregate operations on groups
+    of rows, but they produce a result FOR EACH ROW.
+*/
+
+SELECT * FROM EMPLOYEES;
+
+-- GROUP BY RESULTS
+SELECT department , 
+       AVG(salary) 
+FROM EMPLOYEES 
+GROUP BY department;
+
+-- Windows Function results
+SELECT emp_no,department,
+       AVG(salary) OVER(PARTITION BY department) AS avg_dept
+FROM EMPLOYEES;
+
+
+
