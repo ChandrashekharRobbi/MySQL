ee19394513853433379b466210a8c8192e43618f
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-08-02 16:12:03 +0530

    ORDER BY with Windows ✅

--- Windows Function/ORDER BY with Windows.sql
+++ Windows Function/ORDER BY with Windows.sql
@@ -1,1 +1,15 @@
 -- ORDER BY with Windows
+
+-- it is related to rolling_functions
+
+SELECT department,
+       salary,
+       SUM(salary) OVER(PARTITION BY department ORDER BY salary) AS rolling_sum,
+       SUM(salary) OVER(PARTITION BY department) AS dept_sum
+FROM employees;
+
+SELECT department,
+       salary,
+       SUM(salary) OVER(PARTITION BY department ORDER BY salary DESC) AS rolling_sum,
+       SUM(salary) OVER(PARTITION BY department) AS dept_sum
+FROM employees;
